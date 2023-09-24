a6728914a8b9b68dcb617e3a01438742b428dc19
Conrad Lotz <conrad.lotz@gmail.com>
2023-09-24 14:24:56 -0400

    added streamlit

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import requests
 import json
 from langchain.schema import SystemMessage
 from fastapi import FastAPI
-#import streamlit as st
+import streamlit as st
 
 load_dotenv()
 brwoserless_api_key = os.getenv("BROWSERLESS_API_KEY")
@@ -176,7 +176,6 @@ agent = initialize_agent(
     memory=memory,
 )
 
-'''
 # 4. Use streamlit to create a web app
 def main():
     st.set_page_config(page_title="AI research agent", page_icon=":bird:")
@@ -194,7 +193,7 @@ def main():
 
 if __name__ == '__main__':
     main()
-'''
+
 
 # 5. Set this as an API endpoint via FastAPI
 app = FastAPI()
